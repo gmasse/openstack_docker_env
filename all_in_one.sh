28ed1e99ce8b3c3028109cc708344fe3bfca260c
Germain Masse <gmasse@users.noreply.github.com>
2019-11-05 16:42:04 +0100

    Update all_in_one.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-for commandname in git curl zsh chsh; do
+for commandname in git curl zsh; do
   if ! [ -x "$(command -v $commandname)" ]; then
     echo "Error: $commandname is not installed." >&2
     exit 1
@@ -14,9 +14,17 @@ for filename in ~/.vimrc ~/.zshrc; do
   fi
 done
 
-RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/gmasse/openstack_docker_env/master/customterm.sh)"
-curl -fsSL -o ~/.vimrc https://raw.githubusercontent.com/gmasse/openstack_docker_env/master/vimrc
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 curl -fsSL -o ~/.zshrc https://raw.githubusercontent.com/gmasse/openstack_docker_env/master/zshrc
 
-chsh -s $(command -pv zsh)
+mkdir -p ~/.vim/pack/dist/start/
+mkdir -p ~/.vim/pack/themes/opt/
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+git clone https://github.com/altercation/vim-colors-solarized ~/.vim/pack/themes/opt/vim-colors-solarized
+curl -fsSL -o ~/.vimrc https://raw.githubusercontent.com/gmasse/openstack_docker_env/master/vimrc
+
+#chsh -s $(command -pv zsh)
 #if chsh return PAM error, have a look at https://serverfault.com/questions/202468/changing-the-shell-using-chsh-via-the-command-line-in-a-script
