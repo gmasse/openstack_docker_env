ec5234e65add4e49342b3ac63a903925c8c547c1
Germain Masse <gmasse@users.noreply.github.com>
2019-11-21 17:11:01 +0100

    Fix red

--- custom-ubuntu-motd.sh
+++ custom-ubuntu-motd.sh
@@ -11,7 +11,7 @@ reset="\\\e[0m"
 yellow="\\\e[33m"
 orange="\\\e[91m"
 grey="\\\e[92m"
-red="\\\e[21m"
+red="\\\e[31m"
 printf "#!/bin/sh\n\nprintf \"$yellow\"\n" > /etc/update-motd.d/00-000-color-solarized
 printf "#!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/00-zzz-color-solarized
 printf "#!/bin/sh\n\nprintf \"$orange\"\nhostname | figlet -f mono12\nprintf \"$reset\"\n" > /etc/update-motd.d/05-hostname
