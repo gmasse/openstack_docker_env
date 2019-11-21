1017902c7ebd2765f5dd9aa3140a98211f673972
Germain Masse <gmasse@users.noreply.github.com>
2019-11-21 17:01:28 +0100

    Fix

--- custom-ubuntu-motd.sh
+++ custom-ubuntu-motd.sh
@@ -12,12 +12,12 @@ yellow="\\\e[33m"
 orange="\\\e[91m"
 grey="\\\e[92m"
 red="\\\e[21m"
-printf "#\!/bin/sh\n\nprintf \"$yellow\"\n" > /etc/update-motd.d/00-000-color-solarized
-printf "#\!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/00-zzz-color-solarized
-printf "#\!/bin/sh\n\nprintf \"$orange\"\nhostname | figlet -f mono12\nprintf \"$reset\"\n" > /etc/update-motd.d/05-hostname
-printf "#\!/bin/sh\n\nprintf \"$grey\"\n"   > /etc/update-motd.d/50-000-color-solarized
-printf "#\!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/50-zzz-color-solarized
-printf "#\!/bin/sh\n\nprintf \"$red\"\n"    > /etc/update-motd.d/98-000-color-solarized
-printf "#\!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/98-zzz-color-solarized
+printf "#!/bin/sh\n\nprintf \"$yellow\"\n" > /etc/update-motd.d/00-000-color-solarized
+printf "#!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/00-zzz-color-solarized
+printf "#!/bin/sh\n\nprintf \"$orange\"\nhostname | figlet -f mono12\nprintf \"$reset\"\n" > /etc/update-motd.d/05-hostname
+printf "#!/bin/sh\n\nprintf \"$grey\"\n"   > /etc/update-motd.d/50-000-color-solarized
+printf "#!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/50-zzz-color-solarized
+printf "#!/bin/sh\n\nprintf \"$red\"\n"    > /etc/update-motd.d/98-000-color-solarized
+printf "#!/bin/sh\n\nprintf \"$reset\"\n"  > /etc/update-motd.d/98-zzz-color-solarized
 
 chmod +x /etc/update-motd.d/00-000-color-solarized /etc/update-motd.d/00-zzz-color-solarized /etc/update-motd.d/05-hostname /etc/update-motd.d/50-000-color-solarized /etc/update-motd.d/50-zzz-color-solarized /etc/update-motd.d/98-000-color-solarized /etc/update-motd.d/98-zzz-color-solarized
